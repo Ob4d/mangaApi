a7e99b304fdc9cd0bc12dc3e3f657f8b4d9fa9ae
fantasma mv <83436765+fantasmagg@users.noreply.github.com>
2023-09-26 22:59:45 -0400

    version1

---
+++ MangaApi/Program.cs
@@ -0,0 +1,16 @@
+using MangaApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+var app = builder.Build();
+
+var servicioLogger = (ILogger<Startup>)app.Services.GetService(typeof(ILogger<Startup>));
+
+startup.Configure(app, app.Environment, servicioLogger);
+
+
+app.Run();
---
+++ MangaApi/Startup.cs
@@ -0,0 +1,134 @@
+﻿
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Configuration;
+
+using Microsoft.AspNetCore.Identity;
+
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MangaApi
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Esto es para evitar los clicos y las serializaciones ciclicas NOTA en este caso no las usamos porque estamos usando los DTOs
+            services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles).AddNewtonsoftJson();
+            services.AddControllers()
+               .AddNewtonsoftJson(options =>
+               {
+                   options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+               });
+
+            //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            services.AddControllers();
+            services.AddResponseCaching();
+
+
+            //aqui estamos agregando un esquema de autenticasion basica por default por asi decirlo
+            //JwtBearerDefaults eso tenemos que instalarlo desde nuget, tiene que ser la misma version a todo lo que tengamos instalado
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+            //    opciones => opciones.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuer = false,
+            //        ValidateAudience = false,
+            //        ValidateLifetime = true,
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["llavejwt"])),
+            //        ClockSkew = TimeSpan.Zero
+
+            //    }
+            //    );
+
+
+            // c.SwaggerDoc("v1",  esta primera parte esta "v1" lo que esta haciendo es estableciendo el nombre de la version de nuestra api
+            // eso tiene que ser unico haci evitamos errores en un futuro
+
+
+
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "haaa soy el titulo", Version = "v1" });
+
+                //esto es realmente muy importante, es lo que nos permite verificar nuestro token
+                // para poder ussar la funciones que estan bloquedas por el filtro de autenticacion
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header
+
+
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id= "Bearer"
+                            }
+                        },
+                        new string[] { }
+
+                    }
+                });
+
+            });
+
+            services.AddAutoMapper(typeof(Startup));
+
+
+            //services.AddIdentity<IdentityUser, IdentityRole>()
+            //    .AddEntityFrameworkStores<ApplicationDbContext>()
+            //    .AddDefaultTokenProviders();
+
+
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+
+            if (env.IsDevelopment())
+            {
+
+
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApia v1"));
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();// este lo he tenido que agregar, ya que  vamos a estar reciviendo info del usuario es necesario, porque? nose por ahora
+
+            app.UseEndpoints(endpints =>
+            {
+                endpints.MapControllers();
+            });
+
+
+        }
+
+    }
+}
